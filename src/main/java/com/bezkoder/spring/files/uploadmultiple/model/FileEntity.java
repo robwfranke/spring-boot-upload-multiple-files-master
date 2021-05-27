4e9c2af3fb39002e53bb114ae0f1db010f2d0f05
=Rob Franke <robwfranke@gmail.com>
2021-05-27 09:51:57 +0200

    lombok en FileEntity toegevoegd

---
+++ src/main/java/com/bezkoder/spring/files/uploadmultiple/model/FileEntity.java
@@ -0,0 +1,38 @@
+package com.bezkoder.spring.files.uploadmultiple.model;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+
+@Entity
+@Table(name = "files")
+public class FileEntity {
+
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String name;
+
+    private String contentType;
+
+    private Long size;
+
+    @Lob
+    private byte[] data;
+
+
+}
